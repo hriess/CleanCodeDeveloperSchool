7d379d57cb84153015e33efca2f8f19a225cb598
Hans Riess <hansanton.riess@gmail.com>
2017-07-21 00:04:49 +0200

    Start coding

---
+++ Textumbruch/Textumbruch/PrintTextUmbruch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Textumbruch
+{
+    class PrintTextUmbruch
+    {
+
+        void test()
+        {
+        }
+    }
+}
---
+++ Textumbruch/Textumbruch/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Textumbruch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Out.WriteLine("Original Text:");
+            System.Console.Out.WriteLine(args[0]);
+            System.Console.Out.WriteLine("Text Umbruch mit max. Zeilenlänge von " + args[1] + " Zeichen:");
+
+            var dummy = Console.ReadLine();
+
+        }
+    }
+}
