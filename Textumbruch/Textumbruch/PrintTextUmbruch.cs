ca52f192f1efb6622d39835e7add28df67907f63
hans.riess <hans.riess@socome.de>
2017-07-21 12:11:02 +0200

    Ausgabe entsprechend der Übergabe Parameter

--- Textumbruch/Textumbruch/PrintTextUmbruch.cs
+++ Textumbruch/Textumbruch/PrintTextUmbruch.cs
@@ -9,8 +9,31 @@ namespace Textumbruch
     class PrintTextUmbruch
     {
 
-        void test()
+        public void PrintTextMitUmbruch(String param1, String param2)
         {
+            System.Console.Out.WriteLine("Original Text:");
+            System.Console.Out.WriteLine(param1);
+            System.Console.Out.WriteLine("Text Umbruch mit max. Zeilenlänge von " + param2 + " Zeichen:");
+            TextUmbruch(param1, param2);
+            Console.ReadLine();
+        }
+
+        public void TextUmbruch(String s, String p)
+        {
+            string[] words = s.Split(' ');
+            int length = 0;
+            foreach (string word in words)
+            {
+                Console.Write(word);
+                Console.Write(" ");
+                length = length + word.Length;
+                if (length > int.Parse(p))
+                {
+                    Console.WriteLine("");
+                    length = 0;
+                }
+
+            }
         }
     }
 }
--- Textumbruch/Textumbruch/Program.cs
+++ Textumbruch/Textumbruch/Program.cs
@@ -8,14 +8,32 @@ namespace Textumbruch
 {
     class Program
     {
+        private PrintTextUmbruch ptu = new PrintTextUmbruch();
+        
         static void Main(string[] args)
         {
-            System.Console.Out.WriteLine("Original Text:");
-            System.Console.Out.WriteLine(args[0]);
-            System.Console.Out.WriteLine("Text Umbruch mit max. Zeilenlänge von " + args[1] + " Zeichen:");
+            var p = new Program();
 
-            var dummy = Console.ReadLine();
+            if (args.Length != 2)
+            {
+                p.WrongNumberOfParameter(args);
+            }
+            else
+            {
+                p.PrintText(args);
+            }
+        }
 
+        private void PrintText(string[] args)
+        {
+            ptu.PrintTextMitUmbruch(args[0], args[1]);
+        }
+
+        private void WrongNumberOfParameter(string[] args)
+        {
+            System.Console.Out.WriteLine(args.Length);
+            System.Console.Out.WriteLine("Anzahl Parameter falsch (Text und Länge)");
+            Console.ReadLine();
         }
     }
 }
