1411b77ec818481a47f46b676f9fc90eeab29a00
Hans Riess <hansanton.riess@gmail.com>
2017-08-10 19:49:32 +0200

    Unnoetige verweise entfernt

--- Textumbruch/Textumbruch/WordWrapJava01.cs
+++ Textumbruch/Textumbruch/WordWrapJava01.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 
 // "Es blaut die Nacht,die Sternlein blinken, Schneeflöcklein leis hernieder sinken."
